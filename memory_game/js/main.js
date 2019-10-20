a5060d4dd98167ae2fdc24e6c22207188bb9a1ed
Toni Horne <tonihorne@Tonis-Air.fios-router.home>
2019-10-20 18:46:27 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,26 @@
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cards[0]);
+console.log('User flipped queen');
+
+var cardTwo = cards[1];
+cardsInPlay.push(cards[1]);
+console.log('User flipped queen');
+
+var cardThree = cards[2];
+cardsInPlay.push(cards[2]);
+console.log('Userflipped king');
+
+var cardFour = cards[3];
+cardsInPlay.push(cards[3]);
+console.log('User flipped king');
+
+if (cardsInPlay[0] === cardsInPlay[1])
+ 	alert("You found a match!");
+	else {
+  alert("Sorry, try again");
+}
